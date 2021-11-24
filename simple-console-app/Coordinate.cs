a4dd2e04b6f83902bb52d4623f49e7942e2bca90
Khalid Aqeeli <aqeelikh@gmail.com>
2021-11-24 09:31:04 +0300

    playing with struct and class

---
+++ simple-console-app/Coordinate.cs
@@ -0,0 +1,9 @@
+namespace simple_console_app
+{
+    struct Coordinate
+    {
+        public int x;
+        public int y;
+    }
+
+}
--- simple-console-app/Program.cs
+++ simple-console-app/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using simple_console_app;
+
+namespace CSharpTutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "Hello World!!";
+
+            Console.WriteLine(message);
+
+            var x = new MyClass();
+            x.MyMethod(12,"cool");
+
+            Coordinate point;
+            //Console.Write(point.x); // Compile time error  
+
+            point.x = 10;
+            point.y = 20;
+            Console.WriteLine(point.x); //output: 10  
+            Console.WriteLine(point.y); //output: 20  
+        }
+    }
+}
